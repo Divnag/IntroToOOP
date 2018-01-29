9092d28ed05bec62f9d4093a55351ce4a72b801e
Divnag <divyamn.shine@gmail.com>
2018-01-29 12:00:38 -0500

    added attributes and cat class

--- IntroToOOp/Cat.cs
+++ IntroToOOp/Cat.cs
@@ -8,5 +8,47 @@ namespace IntroToOOp
 {
     class Cat
     {
+        //I need Staes
+        //fields
+        
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+//properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public Cat()
+        {
+            //I need Behaviour
+            //default constructors
+            //takes no parameters
+        }
+
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+
+        }
+
+
+    
+    public void Eat()
+    {
+        if (isHungry)
+        {
+            isHungry = false;
+        }
+
+        Console.WriteLine("Is the cat hungry? " + isHungry);
     }
+
+}
 }
